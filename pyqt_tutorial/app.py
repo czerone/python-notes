7e79fe08098a1c5b1fbdf4d65a18b3f1931a8479
Poting Chen <czerone1011@gmail.com>
2020-06-25 14:49:18 +0800

    add file name start function on file name encryption window

--- pyqt_tutorial/app.py
+++ pyqt_tutorial/app.py
@@ -1,24 +1,56 @@
 import sys
-from PyQt5.QtWidgets import QDialog, QApplication
-from MyFirstUI import Ui_Form
+from PyQt5.QtWidgets import QDialog, QMainWindow, QWidget, QApplication
+from LoginPageUI import Ui_LoginProfile
+from FileNameEncryption import Ui_FileEncryption
 
-class AppWindow(QDialog):
+
+class AppWindow(QMainWindow):
     def __init__(self):
         super(self.__class__, self).__init__()
-        self.ui = Ui_Form()
+        self.ui =Ui_FileEncryption()
         self.ui.setupUi(self)
 
-
         # click event function will be shown under ui
-        self.ui.pushButton.clicked.connect(self.pushButtonClick)
+        self.ui.start_button.clicked.connect(self.changeName)
         self.show()
 
-    def pushButtonClick(self):
-        self.ui.label.setText("Hello World")
+    def changeName(self):
+        input_file_name = self.ui.input_file_name.text()
+        input_file_address = self.ui.input_file_address.text()
+        sequence = self.ui.sequence.text()
+        self.ui.result.setText("Change Name Success")
+
+
+
+class LoginWindow(QWidget):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.ui = Ui_LoginProfile()
+        self.ui.setupUi(self)
+
+        # click event function will be shown under ui
+        self.ui.login_button.clicked.connect(self.loginCheck)
+        self.show()
+
+    def loginCheck(self):
+        input_account = self.ui.input_account.text()
+        input_password = self.ui.input_password.text()
+        if input_password == 'chen' and input_account == 'poting':
+            self.ui.result.setText("Success")
+            self.slot_btn_function()
+        else:
+            self.ui.result.setText("Failed")
+    def slot_btn_function(self):
+        self.hide()
+        self.a = AppWindow()
+        self.a.show()
+
+def main():
+    app = QApplication(sys.argv)
+    w = LoginWindow()
+    w.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    w = AppWindow()
-    w.show()
-    sys.exit(app.exec_())
+    main()
