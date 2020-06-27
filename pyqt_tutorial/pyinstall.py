e02f635f13ac05db9ba68bf33663c895937630e4
Poting Chen <czerone1011@gmail.com>
2020-06-27 11:35:15 +0800

    update

--- pyqt_tutorial/pyinstall.py
+++ pyqt_tutorial/pyinstall.py
@@ -2,5 +2,5 @@ import os
 
 if __name__ == '__main__':
     from PyInstaller.__main__ import run
-    opts=['app.py', '-w', '-F', '--icon=icon.ico', '--noupx', '--clean']
+    opts=['app.py', '--hidden-import=PyQt5.sip', '-F', '--icon=icon.ico', '--noupx', '--clean', '--paths=C:\ProgramData\Anaconda3\Lib\site-packages\PyQt5']
     run(opts)
